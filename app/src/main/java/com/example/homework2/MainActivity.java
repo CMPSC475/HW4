9de8ef4189ba13e3bf8d0f86abe1a1a00e842362
spf100 <72419892+spf100@users.noreply.github.com>
2021-02-24 15:23:19 -0500

    updates

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.homework2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,15 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
 
+import javax.security.auth.Subject;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +32,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        final TextView To = findViewById(R.id.textViewTo);
-        final TextView TextViewSubject = findViewById(R.id.TextViewSubject);
-        final CheckBox GetMin = findViewById(R.id.getMin_Checkbox);
-        final CheckBox Search = findViewById(R.id.search_Checkbox);
-        final CheckBox Insert = findViewById(R.id.insert_CheckBox);
-        final TextView SubjectTextView = findViewById(R.id.TextViewSubject);
-        //RadioGroup RadioGroupCases = findViewById(R.id.RadioGroup);
+    //@Override
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_compose:
+                sendEmail();
+                item.setIcon(R.drawable.ic_baseline_email_24);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sendEmail() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto: "));
+        String emailAddress = ((TextView) findViewById(R.id.editText_Email_Address)).getText().toString();
+        String emailSubject = ((TextView) findViewById(R.id.editText_email_subject)).getText().toString();
+        String emailContent = ((TextView) findViewById(R.id.textViewEmail)).getText().toString();
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {emailAddress});
+        intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailContent);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        String spinnertext = spinner.getSelectedItem().toString();
+        String subjectText = "Subject: " + emailSubject + "\n";
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        String [][][] runtime = {
+                {{"O(log n)", "O(log n)", "O(log n)"},  // worstcase 2-3 Tree
+                        {"O(log n)", "O(log n)", "O(log n)"}}, // averagecase 2-3 Tree
+
+                {{"O(n)", "O(n)", "O(n)"},  // worstcase BST
+                        {"O(log n)", "O(log n)", "O(log n)"}}, //averagecase BST
+
+                {{"O(n)", "O(n)", "O(n)"},  // worstcase Hash Table
+                        {"O(n)", "O(1)", "O(1)"}}, //averagecase Hash Table
+
+                {{"O(n)", "O(1)", "O(n)"},  // worstcase Linked List
+                        {"O(n)", "O(1)", "O(n)"}}, // averagecase Linked List
+
+                {{"O(1)", "O(log n)", "O(n)"},  // worstcase Min Heap
+                        {"O(1)", "O(log n)", "O(n)"}}}; // averagecase Min Heap
+
+        StringBuilder result = new StringBuilder();
+        CheckBox GetMin = findViewById(R.id.getMin_Checkbox);
+        CheckBox Insert = findViewById(R.id.insert_CheckBox);
+        CheckBox Search = findViewById(R.id.search_Checkbox);
         final RadioButton AverageCase = findViewById(R.id.averageCase_RadioButton);
         final RadioButton WorstCase = findViewById(R.id.worstCase_RadioButton);
-        Spinner spinner = findViewById(R.id.spinner);
-        TextViewSubject.setText(getString(R.string.subject));
-        GetMin.setText(getString(R.string.getMin));
-        Search.setText(getString(R.string.search));
-
+        int casetest = 0;
         AverageCase.setOnClickListener(view -> {
             AverageCase.setChecked(true);
-
             if (WorstCase.isChecked()) {
                 WorstCase.setChecked(false);
             }
@@ -56,185 +108,90 @@ public class MainActivity extends AppCompatActivity {
                 WorstCase.setChecked(false);
             }
         });
+        result.append(emailSubject).append("\n");
+        if (WorstCase.isChecked()) {
+            result.append("Worst Case Time Complexity for ").append(spinnertext).append(":\n");
+            casetest = 0;
 
-
-        fab.setOnClickListener(view -> {
-            TextView EmailAddress = findViewById(R.id.editText_Email_Address);
-            TextView EmailSubject = findViewById(R.id.editText_email_subject);
-            String spinnertext = spinner.getSelectedItem().toString();
-            String totext = "To: " + EmailAddress.getText().toString();
-            String subjectText = "Subject: " + EmailSubject.getText().toString() + "\n";
-            final String[] radioText = {""};
-            final int[] casetest = {0};
-            casetest[0] = 2;
-
-            final boolean[] changed = {false};
-            /*RadioGroup radioGroup = findViewById(R.id.RadioGroup);
-            radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-                if (checkedId == R.id.worstCase_RadioButton) {
-                    radioText[0] = "Worst Case Time Complexity for " + spinnertext + ":";
-                    casetest[0] = 1;
-                    changed[0] = true;
-
-                } else if (checkedId == R.id.averageCase_RadioButton) {
-                    radioText[0] = "Average Case Time Complexity for " + spinnertext + ":";
-                    casetest[0] = 2;
-                    changed[0] = true;
-                }
-            });*/
-
-            if (WorstCase.isChecked()) {
-                radioText[0] = "Worst Case Time Complexity for " + spinnertext + ":";
-            } else if (AverageCase.isChecked()) {
-                radioText[0] = "Average Case Time Complexity for " + spinnertext + ":";
-            }
-
-
-//            if (!changed[0]) {    //case 1 is worst case 2 is average
-//                radioText[0] = "Average Case Time Complexity for " + spinnertext + ":";
-//            }
-
-            if (spinnertext.equals("2-3 Tree")) {
-                subjectText += radioText[0];
-                if (casetest[0] == 1) {
-
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(Log(n))";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(Log(n))";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(Log(n))";
-                    }
-                } else if (casetest[0] == 2) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(Log(n))";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(Log(n))";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + " O(Log(n))";
-                    }
-                }
-            } else if (spinnertext.equals("Binary Search Tree")) {
-                subjectText += radioText[0];
-                if (casetest[0] == 1) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(n)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(n)";
-                    }
-
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(n)";
-                    }
-                } else if (casetest[0] == 2) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(Log(n))";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(Log(n))";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(Log(n))";
-                    }
-                }
-            } else if (spinnertext.equals("Hash Table")) {
-                subjectText += radioText[0];
-                if (casetest[0] == 1) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(n)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(n)";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(n)";
-                    }
-                } else if (casetest[0] == 2) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(1)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(1)";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(1)";
-                    }
-                }
-            } else if (spinnertext.equals("Linked List")) {
-                subjectText += radioText[0];
-                if (casetest[0] == 1) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(n)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(1)";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(n)";
-                    }
-                } else if (casetest[0] == 2) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(n)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(1)";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(n)";
-                    }
-                }
-
-            } else if (spinnertext.equals("Min Heap")) {
-                subjectText += radioText[0];
-                if (casetest[0] == 1) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(1)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(n)";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(n)";
-                    }
-                } else if (casetest[0] == 2) {
-                    if (GetMin.isChecked()) {
-                        subjectText += "\nGet Minimum: " + "O(1)";
-                    }
-                    if (Insert.isChecked()) {
-                        subjectText += "\nInsert: " + "O(n)";
-                    }
-                    if (Search.isChecked()) {
-                        subjectText += "\nSearch: " + "O(n)";
-                    }
-                }
-
-            }
-            To.setText(totext);
-            SubjectTextView.setText(subjectText);
-        });
-    }
-
-
-    //@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
+        } else if (AverageCase.isChecked()) {
+            result.append("Average Case Time Complexity for ").append(spinnertext).append(":\n");
+            casetest = 1;
         }
-        return super.onOptionsItemSelected(item);
+
+        int spinnercase;
+        switch(spinnertext){
+            case "2-3 Tree":
+                spinnercase = 0;
+                if (GetMin.isChecked()) {
+                    result.append("Get Minimum: ").append(runtime[spinnercase][casetest][0]).append("\n");
+                }
+                if (Insert.isChecked()) {
+                    result.append("Insert: ").append(runtime[spinnercase][casetest][1]).append("\n");
+                }
+                if (Search.isChecked()) {
+                    result.append("Search: ").append(runtime[spinnercase][casetest][2]).append("\n");
+                }
+                break;
+            case "Binary Search Tree":
+                spinnercase = 1;
+                if (GetMin.isChecked()) {
+                    result.append("Get Minimum: ").append(runtime[spinnercase][casetest][0]).append("\n");
+                }
+                if (Insert.isChecked()) {
+                    result.append("Insert: ").append(runtime[spinnercase][casetest][1]).append("\n");
+                }
+                if (Search.isChecked()) {
+                    result.append("Search: ").append(runtime[spinnercase][casetest][2]).append("\n");
+                }
+                break;
+            case "Hash Table":
+                spinnercase = 2;
+                if (GetMin.isChecked()) {
+                    result.append("Get Minimum: ").append(runtime[spinnercase][casetest][0]).append("\n");
+                }
+                if (Insert.isChecked()) {
+                    result.append("Insert: ").append(runtime[spinnercase][casetest][1]).append("\n");
+                }
+                if (Search.isChecked()) {
+                    result.append("Search: ").append(runtime[spinnercase][casetest][2]).append("\n");
+                }
+                break;
+            case "Linked List":
+                spinnercase = 3;
+                if (GetMin.isChecked()) {
+                    result.append("Get Minimum: ").append(runtime[spinnercase][casetest][0]).append("\n");
+                }
+                if (Insert.isChecked()) {
+                    result.append("Insert: ").append(runtime[spinnercase][casetest][1]).append("\n");
+                }
+                if (Search.isChecked()) {
+                    result.append("Search: ").append(runtime[spinnercase][casetest][2]).append("\n");
+                }
+                break;
+            case "Min Heap":
+                spinnercase = 4;
+                if (GetMin.isChecked()) {
+                    result.append("Get Minimum: ").append(runtime[spinnercase][casetest][0]).append("\n");
+                }
+                if (Insert.isChecked()) {
+                    result.append("Insert: ").append(runtime[spinnercase][casetest][1]).append("\n");
+                }
+                if (Search.isChecked()) {
+                    result.append("Search: ").append(runtime[spinnercase][casetest][2]).append("\n");
+                }
+                break;
+        }
+
+        final TextView To = findViewById(R.id.textViewTo);
+        final TextView SubjectTextView = findViewById(R.id.TextViewSubject);
+        final String tostring = getString(R.string.to) +" "+ emailAddress;
+        final String Subjecttext = getString(R.string.subject)+" " + result;
+        To.setText(tostring);
+        SubjectTextView.setText(Subjecttext);
+
     }
+
+
 }
 
 
